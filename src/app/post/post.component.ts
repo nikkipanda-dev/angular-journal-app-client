269d2edb33d435bbb41bf76b6ded95885a47e851
Nikki <62213020+nikkipanda-dev@users.noreply.github.com>
2022-03-21 02:47:06 +0800

    Init destroy post

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -16,32 +16,46 @@ export class PostComponent implements OnInit {
     }
 
     getData() {
-        (document.getElementById('modal-body') as HTMLDivElement).innerHTML = ''
+        (document.getElementById('modal-body') as HTMLDivElement).innerHTML = '';
+
+        console.log('this ', this)
 
         const editForm = document.createElement('div');
-        editForm.innerHTML = `<div class="container"><div class="mb-3"><label for="update-title" class="form-label">Title</label><input type='hidden' name='user_id' id="user-id" value="${this.post.id}" /><input type="text" class="form-control" id="update-title" value="${this.post.title}"><span id="update-title-error"></span></div><div class="mb-3"><label for="update-message" class="form-label">Body</label><textarea class="form-control" id="update-message" rows="3" value="${this.post.body}">${this.post.body}</textarea><span id="update-message-error"></span></div><div class="mb-3"><button type='button' class="btn btn-sm btn-secondary" id="update-button">Save</button></div></div>`;
+        editForm.innerHTML = `<div class="container"><div class="mb-3"><label for="update-title" class="form-label">Title</label><input type='hidden' name='post_id' id="post-id" value="${this.post.id}" /><input type="text" class="form-control" id="update-title" value="${this.post.title}"><span id="update-title-error"></span></div><div class="mb-3"><label for="update-message" class="form-label">Body</label><textarea class="form-control" id="update-message" rows="3" value="${this.post.body}">${this.post.body}</textarea><span id="update-message-error"></span></div><div class="mb-3"><button type='button' class="btn btn-sm btn-secondary" id="update-button">Save</button></div></div>`;
 
         (document.getElementById('modal-body') as HTMLDivElement).appendChild(editForm);
         (document.getElementById('update-button') as HTMLButtonElement).addEventListener('click', this.updatePost);
     }
 
+    showAlert() {
+        (document.getElementById('modal-body') as HTMLDivElement).innerHTML = '';
+
+        console.log('this ', this)
+
+        const deleteAlert = document.createElement('div');
+        deleteAlert.innerHTML = `<div class="alert alert-danger" role="alert">You are about to delete this post. Continue?</div><input type='hidden' name='post_id' id="post-id" value="${this.post.id}" /><div class="mb-3 d-flex flex-column flex-sm-row justify-content-center align-items-stretch align-items-sm-center"><button type='button' class="btn btn-sm btn-danger" id="delete-button">Delete</button></div>`;
+
+        (document.getElementById('modal-body') as HTMLDivElement).appendChild(deleteAlert);
+        (document.getElementById('delete-button') as HTMLButtonElement).addEventListener('click', this.deletePost);
+    }
+
     updatePost() {
-        let userId = null;
+        let postId = null;
         let title = null;
         let body = null;
 
-        if (document.getElementById('user-id') && document.getElementById('update-title') && document.getElementById('update-message')) {
+        if (document.getElementById('post-id') && document.getElementById('update-title') && document.getElementById('update-message')) {
             (document.getElementById('update-title-error') as HTMLInputElement).innerHTML = '';
             (document.getElementById('update-message-error') as HTMLInputElement).innerHTML = '';
 
-            userId = (document.getElementById('user-id') as HTMLInputElement).value;
+            postId = (document.getElementById('post-id') as HTMLInputElement).value;
             title = (document.getElementById('update-title') as HTMLInputElement).value;
             body = (document.getElementById('update-message') as HTMLInputElement).value;
         }
 
-        if (title && body) {
+        if (postId && title && body) {
             axios.post('http://localhost:8000/api/update', {
-                id: userId,
+                id: postId,
                 title: title,
                 body: body,
             }, {
@@ -66,12 +80,54 @@ export class PostComponent implements OnInit {
                     console.log('err ', err.response ? err.response : '');
                 })
         } else {
+            if (!(postId)) {
+                console.log('Invalid post ID provided.');
+            }
+
             if (!(title)) {
                 (document.getElementById('update-title-error') as HTMLInputElement).innerHTML = 'Title error';
             }
 
             if (!(body)) {
                 (document.getElementById('update-message-error') as HTMLInputElement).innerHTML = 'Body error';
+            }
+        }
+    }
+
+    deletePost() {
+        let postId = null;
+
+        if (document.getElementById('post-id')) {
+            postId = (document.getElementById('post-id') as HTMLInputElement).value;
+        }
+
+        if (postId) {
+            axios.post('http://localhost:8000/api/destroy', {
+                id: postId,
+            }, {
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json',
+                    'Access-Control-Allow-Origin': '*',
+                },
+                withCredentials: true,
+            })
+
+                .then(response => {
+                    if (response.data.isSuccess) {
+                        location.reload();
+                        window.scrollTo(0, 0);
+                    } else {
+                        console.log('post err ', response.data.errorText);
+                    }
+                })
+
+                .catch(err => {
+                    console.log('err ', err.response ? err.response : '');
+                })
+        } else {
+            if (!(postId)) {
+                console.log('Invalid post ID provided.');
             }
         }
     }
